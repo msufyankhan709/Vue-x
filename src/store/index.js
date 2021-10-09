e70b212f5b9643a82330d2c1e3c3f372d934127f
M Sufyan Khan <msufyankhan709@gmail.com>
2021-10-09 11:01:18 +0500

    First commit

---
+++ src/store/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import {dataService} from '../shared'
+import {GET_HEROES} from './mutation-types'
+
+Vue.use(Vuex);
+
+const state={
+  heroes:[],
+};
+const mutations={
+  [GET_HEROES](state,heroes){
+    state.heroes=heroes;
+  }
+};
+const actions={
+  async getHeroesAction({commit}){
+    const heroes=await dataService.getHeroes();
+    commit(GET_HEROES, heroes);
+  }
+};
+const getters={};
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  getters,
+})
